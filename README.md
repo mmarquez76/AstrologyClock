d89cb9680e3498fc49e97d85e183b02e88802556
robert mermet <robertmermet@gmail.com>
2018-05-30 14:41:39 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Astrology Clock
+
+Displays the time and sun sign.
+
+Uses Astro font type by Cosmorama.
+
+    git clone git@github.com:robertmermet/astrologyclock.git
+
+>**demo** [robertmermet.com/projects/astrologyclock](http://robertmermet.com/projects/astrologyclock)
+
+>**author** [robertmermet.com](http://robertmermet.com)
---
+++ clock.js
@@ -0,0 +1,334 @@
+window.addEventListener('load', function load() {
+
+    window.removeEventListener('load', load, false);
+
+    var canvas = document.createElement('canvas'),
+        ctx    = canvas.getContext('2d'),
+        signs  = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L'],
+        radius;
+
+    document.body.appendChild(canvas);
+    window.addEventListener('resize', resize);
+
+    resize();
+    setInterval(drawClock, 60);
+
+    function resize() {
+        radius = getCircleRadius(20);
+        ctx.canvas.width  = window.innerWidth;
+        ctx.canvas.height = window.innerHeight;
+        ctx.translate(canvas.width / 2, canvas.height / 2);
+        drawClock();
+    }
+
+     function getCircleRadius(padding) {
+        if (window.innerWidth < window.innerHeight) {
+            return Math.round((window.innerWidth / 2) - padding);
+        } else {
+            return Math.round((window.innerHeight / 2) - padding);
+        }
+     }
+
+    function drawClock() {
+        var date = new Date(),
+            sign = getSign(date);
+        drawFace();
+        drawNumerals(date, sign);
+        drawTime(date, sign);
+    }
+
+    function drawFace() {
+        var angle;
+        ctx.fillStyle = '#fff';
+        ctx.fillRect(-canvas.width, -canvas.height, canvas.width * 2, canvas.height * 2);
+        ctx.beginPath();
+        ctx.arc(0, 0, radius, 0, 2 * Math.PI);
+        ctx.fillStyle = '#fff';
+        ctx.fill();
+        ctx.lineWidth = 2;
+        ctx.strokeStyle = '#eee';
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.arc(0, 0, radius * .4, 0, 2 * Math.PI);
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.arc(0, 0, radius * .7, 0, 2 * Math.PI);
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.arc(0, 0, radius * .9, 0, 2 * Math.PI);
+        ctx.stroke();
+        // Indices for Signs
+        for (var i = 0; i < 12; i++) {
+            angle = (i + .5) * Math.PI / 6;
+            ctx.rotate(angle);
+            ctx.moveTo(radius * .4, 0)
+            ctx.lineTo(radius * .7, 0);
+            ctx.stroke();
+            ctx.rotate(-angle);
+        }
+        // Indices for Hours
+        for (var i = 0; i < 24; i++) {
+            angle = (i + .5) * Math.PI / 12;
+            ctx.rotate(angle);
+            ctx.moveTo(radius * .7, 0)
+            ctx.lineTo(radius * .9, 0);
+            ctx.stroke();
+            ctx.rotate(-angle);
+        }
+        // Indices for Minutes / Seconds
+        for (var i = 0; i < 60; i++) {
+            angle = (i + .5) * Math.PI / 30;
+            ctx.rotate(angle);
+            ctx.moveTo(radius * .9, 0)
+            ctx.lineTo(radius, 0);
+            ctx.stroke();
+            ctx.rotate(-angle);
+        }
+    }
+
+    function drawNumerals(date, sign) {
+        let hour   = date.getHours(),
+            minute = date.getMinutes(),
+            angle;
+        ctx.textBaseline = 'middle';
+        ctx.textAlign = 'center'
+        ctx.fillStyle = '#ddd';
+        ctx.font = radius * 0.12 + 'px Astro';
+        // 12 signs
+        for (var i = 1; i < 13; i++) {
+            angle = i * Math.PI / 6;
+            ctx.fillStyle = (i === Math.floor(sign)) ? '#999' : '#ddd';
+            ctx.rotate(angle);
+            ctx.translate(0, -radius * 0.55);
+            ctx.rotate(-angle);
+            ctx.fillText(signs[i - 1], 0, 0);
+            ctx.rotate(angle);
+            ctx.translate(0, radius * 0.55);
+            ctx.rotate(-angle);
+        }
+        // 24 hours
+        ctx.font = radius * 0.12 + 'px Astro';
+        for (var i = 1; i < 25; i++) {
+            angle = i * Math.PI / 12;
+            ctx.fillStyle = (i === hour) ? '#999' : '#ddd';
+            ctx.rotate(angle);
+            ctx.translate(0, -radius * 0.8);
+            ctx.rotate(-angle);
+            ctx.fillText(String(i), 0, 0);
+            ctx.rotate(angle);
+            ctx.translate(0, radius * 0.8);
+            ctx.rotate(-angle);
+        }
+        // 60 minutes / seconds
+        ctx.font = radius * 0.04 + 'px arial';
+        for (var i = 1; i < 61; i++) {
+            angle = i * Math.PI / 30;
+            ctx.fillStyle = (i === minute) ? '#999' : '#ddd';
+            ctx.rotate(angle);
+            ctx.translate(0, -radius * 0.95);
+            ctx.rotate(-angle);
+            ctx.fillText(String(i), 0, 0);
+            ctx.rotate(angle);
+            ctx.translate(0, radius * 0.95);
+            ctx.rotate(-angle);
+        }
+
+    }
+
+    function drawTime(date, sign) {
+        var hour     = date.getHours(),
+            minute   = date.getMinutes(),
+            second   = date.getSeconds(),
+            millisec = date.getMilliseconds();
+        // sign
+        sign = ((sign - .5) * Math.PI / 6);
+        ctx.strokeStyle = '#ddd';
+        drawHand(ctx, sign, radius * 0.4, radius * 0.005);
+        // hour
+        hour = ((hour - 0.5) * Math.PI / 12) +
+               (minute * Math.PI / (12 * 60)) +
+               (second * Math.PI / (720 * 60));
+        ctx.strokeStyle = '#ccc';
+        drawHand(ctx, hour, radius * 0.7, radius * 0.004);
+        // minute
+        minute = ((minute - 0.5) * Math.PI / 30) +
+                 (second * Math.PI / (30 * 60)) +
+                 (millisec * Math.PI / (30 * 60000));
+        ctx.strokeStyle = '#bbb';
+        drawHand(ctx, minute, radius * 0.9, radius * 0.003);
+        // second
+        second = ((second + 0.5) * Math.PI / 30) + (millisec * Math.PI / (30 * 1000));
+        ctx.strokeStyle = '#aaa';
+        drawHand(ctx, second, radius, radius * 0.002);
+    }
+
+    function drawHand(ctx, pos, length, width) {
+        ctx.lineWidth = width;
+        ctx.lineCap   = 'round';
+        ctx.beginPath();
+        ctx.moveTo(0, 0);
+        ctx.rotate(pos);
+        ctx.lineTo(0, -length);
+        ctx.stroke();
+        ctx.rotate(-pos);
+    }
+
+    function getSign(date) {
+        var month = date.getMonth(),
+            day   = date.getDate(),
+            sign;
+        switch (month) {
+            case 0:  // Jan
+                if (day <= 19) {
+                    sign = 10; // Capricon
+                    // Dec 22 - Jan 19
+                    // 9 (Dec) + 19 (Jan) = 28
+                    sign += (22 - day) / 28;
+                } else {
+                    sign = 11; // Aquarius
+                    // 11 (Jan) + 18 (Feb) = 29
+                    sign += (day + 11) / 29;
+                }
+                break;
+            case 1:  // Feb
+                if (day <= 18) {
+                    sign = 11; // Aquarius
+                    // Jan 20 - Feb 18
+                    // 11 (Jan) + 18 (Feb) = 29
+                    sign += (20 - day) / 29;
+                } else {
+                    sign = 12; // Pisces
+                    // 9/10 (Feb) + 20 (Mar) = 29/30
+                    var leapYear = isLeapYear(date.getYear());
+                    sign += (day + (leapYear ? 9 : 10)) / (leapYear ? 29 : 28);
+                }
+                break;
+            case 2:  // Mar
+                if (day <= 20) {
+                    sign = 12; // Pisces
+                    // Feb 19 - Mar 20
+                    // 9/10 (Feb) + 20 (Mar) = 29/30
+                    sign += (19 - day) / (isLeapYear(date.getYear()) ? 29 : 28);
+                } else {
+                    sign = 1;  // Aries
+                    // 10 (Mar) + 19 (Apr) = 29
+                    sign += (day + 10) / 29;
+                }
+                break;
+            case 3:  // Apr
+                if (day <= 19) {
+                    sign = 1;  // Aries
+                    // Mar 21 - Apr 19
+                    // 10 (Mar) + 19 (Apr) = 29
+                    sign += (21 - day) / 29;
+                } else {
+                    sign = 2;  // Taurus
+                    // 10 (Apr) + 20 (May) = 30
+                    sign += (day + 10) / 30;
+                }
+                break;
+            case 4:  // May
+                if (day <= 20) {
+                    sign = 2;  // Taurus
+                    // Apr 20 - May 20
+                    // 10 (Apr) + 20 (May) = 30
+                    sign += (20 - day) / 30;
+                } else {
+                    sign = 3;  // Gemini
+                    // 10 (May) + 20 (Jun) = 30
+                    sign += (day + 10) / 30;
+                }
+                break;
+            case 5:  // Jun
+                if (day <= 20) {
+                    sign = 3;  // Gemini
+                    // May 21 - Jun 20
+                    // 10 (May) + 20 (Jun) = 30
+                    sign += (21 - day) / 30;
+                } else {
+                    sign = 4;  // Cancer
+                    // 9 (Jun) + 22 (Jul) = 31
+                    sign += (day + 9) / 31;
+                }
+                break;
+            case 6:  // Jul
+                if (day <= 22) {
+                    sign = 4;  // Cancer
+                    // Jun 21 - Jul 22
+                    // 9 (Jun) + 22 (Jul) = 31
+                    sign += (21 - day) / 31;
+                } else {
+                    sign = 5;  // Leo
+                    // 7 (Jul) + 22 (Aug) = 29
+                    sign += (day + 7) / 29;
+                }
+                break;
+            case 7:  // Aug
+                if (day <= 22) {
+                    sign = 5;  // Leo
+                    // Jul 23 - Aug 22
+                    // 7 (Jul) + 22 (Aug) = 29
+                    sign += (23 - day) / 29;
+                } else {
+                    sign = 6;  // Virgo
+                    // 8 (Aug) + 22 (Sep) = 30
+                    sign += (day + 8) / 30;
+                }
+                break;
+            case 8:  // Sep
+                if (day <= 22) {
+                    sign = 6;  // Virgo
+                    // Aug 23 - Sep 22
+                    // 8 (Aug) + 22 (Sep) = 30
+                    sign += (23 - day) / 30;
+                } else {
+                    sign = 7;  // Libra
+                    // 8 (Sep) + 22 (Oct) = 30
+                    sign += (day + 8) / 30;
+                }
+                break;
+            case 9:  // Oct
+                if (day <= 22) {
+                    sign = 7;  // Libra
+                    // Sep 23 - Oct 22
+                    // 8 (Sep) + 22 (Oct) = 30
+                    sign += (23 - day) / 30;
+                } else {
+                    sign = 8;  // Scorpio
+                    // 8 (Oct) + 21 (Nov) = 29
+                    sign += (day + 8) / 29;
+                }
+                break;
+            case 10: // Nov
+                if (day <= 21) {
+                    sign = 8;  // Scorpio
+                    // Oct 23 - Nov 21
+                    // 8 (Oct) + 21 (Nov) = 29
+                    sign += (23 - day) / 29;
+                } else {
+                    sign = 9;  // Sagittarius
+                    // 8 (Nov) + 21 (Dec) = 29
+                    sign += (day + 8) / 29;
+                }
+                break;
+            case 11: // Dec
+                if (day <= 21) {
+                    sign = 9;  // Sagittarius
+                    // Nov 22 - Dec 21
+                    // 8 (Nov) + 21 (Dec) = 29
+                    sign += (22 - day) / 29;
+                } else {
+                    sign = 10; // Capicorn
+                    // 9 (Dec) + 19 (Jan) = 28
+                    sign += (day + 9) / 28;
+                }
+        }
+        return sign;
+    }
+
+    function isLeapYear(year) {
+        return (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) ? true : false;
+    }
+
+
+}, true);
